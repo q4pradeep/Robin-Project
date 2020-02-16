7582998ab7ba1619421759939586745870230c65
Pradeep <q4pradeep@gmail.com>
2020-02-16 12:22:21 +0000

    removed node modules

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import {NbButtonModule, NbCardModule, NbMenuModule} from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   imports: [
@@ -12,9 +14,13 @@ import { PagesRoutingModule } from './pages-routing.module';
     ThemeModule,
     NbMenuModule,
     DashboardModule,
+    NbCardModule,
+    NbButtonModule,
   ],
   declarations: [
     PagesComponent,
+    HomeComponent,
+    UserComponent,
   ],
 })
 export class PagesModule {
